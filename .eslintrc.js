760ee1f8eccbf8cd8aafa23e2b417f65dfd8a2d4
John Farrell MacDonald <john@jfmacdonald.com>
2020-11-19 15:13:26 -0800

    npm install --save-dev eslint-config-recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true,
     es6: true,
   },
   plugins: ["prettier"],
-  extends: ["plugin:prettier/recommended"],
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
